734270ae5d095326031d89ce04419a10be3aa864
DucHuynh <duchuynhpl1@gmail.com>
2022-11-12 16:53:22 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<?php require "layout/header.php"; ?>
+
+<div class="heading">
+    <h1>về chúng tôi</h1>
+    <p> <a href="index.html">trang chủ>></a> về chúng tôi </p>
+</div>
+
+<section class="about">
+
+    <div class="image">
+        <img src="image/banner-7.webp" alt="">
+    </div>
+
+    <div class="content">
+        <span>Chào mừng đến với cửa hàng của chúng tôi</span>
+        <h3>Thời trang nam và phụ kiện</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel sequi nostrum quae nobis non quaerat nisi voluptatibus recusandae reprehenderit tempore eligendi, eum quibusdam perferendis dicta, incidunt dolores nemo ex.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem cumque molestiae blanditiis deleniti aspernatur, ab tempora quisquam sapiente commodi hic.</p>
+        <a href="#" class="btn">đọc thêm</a>
+    </div>
+
+</section>
+
+<section class="gallery">
+
+    <h1 class="title"> xem <span>ảnh</span> <a href="#">xem tất cả >></a> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/gallery-img-11.jpg" alt="ao thun">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/gallery-img-12.webp" alt="ao thun">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/gallery-img-13.jpg" alt="giay nam">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/gallery-img-14.jpg" alt="[hu kien">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/gallery-img-15.webp" alt="quan jogger">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/gallery-img-16.jpg" alt="ao khoac">
+            <div class="icons">
+                <a href="#" class="fas fa-eye"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share-alt"></a>
+            </div>
+        </div>
+
+        
+
+    </div>
+
+</section>
+
+<?php require "layout/footer.php"; ?>
+
+<!-- custom css file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
